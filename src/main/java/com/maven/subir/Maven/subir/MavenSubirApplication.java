3459f763ffc219a04b715b7ec49ada8756bc5b87
APPCO\edgar.delarocha <edgar.delarocha@microsip.com>
2023-06-20 13:09:34 -0600

    first commit

---
+++ src/main/java/com/maven/subir/Maven/subir/MavenSubirApplication.java
@@ -0,0 +1,13 @@
+package com.maven.subir.Maven.subir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenSubirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenSubirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maven/subir/Maven/subir/MavenSubirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maven.subir.Maven.subir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenSubirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
